89392196101a7646662c5b2d3492443a8e8e9013
Sean McClure <seanmcclure@Seans-MacBook-Pro.local>
2018-08-10 08:41:06 -0600

    adding D3 and animations

--- deploy/app/scripts/fetch_data.js
+++ deploy/app/scripts/fetch_data.js
@@ -1,4 +1,4 @@
- bar_data = [{
+        bar_data_a = [{
                 "name": "PASSENGER AGE",
                 "value": 3,
         },
@@ -15,7 +15,61 @@
                 "value": 5,
         }];
 
-store_data('browser', 1, {
-"key" : "bar_data",
-"value" : bar_data
-})
+         bar_data_b = [{
+                "name": "PASSENGER AGE",
+                "value": 13,
+        },
+            {
+                "name": "SIBLINGS / SPOUSES",
+                "value": 2,
+        },
+            {
+                "name": "PARENTS / CHILDREN",
+                "value": 8,
+        },
+            {
+                "name": "FARE PAID",
+                "value": 2,
+        }];
+
+         bar_data_c = [{
+                "name": "PASSENGER AGE",
+                "value": 5,
+        },
+            {
+                "name": "SIBLINGS / SPOUSES",
+                "value": 1,
+        },
+            {
+                "name": "PARENTS / CHILDREN",
+                "value": 9,
+        },
+            {
+                "name": "FARE PAID",
+                "value": 5,
+        }];
+
+         bar_data_d = [{
+                "name": "PASSENGER AGE",
+                "value": 6,
+        },
+            {
+                "name": "SIBLINGS / SPOUSES",
+                "value": 2,
+        },
+            {
+                "name": "PARENTS / CHILDREN",
+                "value": 7,
+        },
+            {
+                "name": "FARE PAID",
+                "value": 8,
+        }];
+
+setTimeout(function() {
+$('.bar_chart_frame')[0].contentWindow.draw_chart(eval('bar_data_a'))
+}, 1000)
+
+setTimeout(function() {
+$('.bar_chart_frame_b')[0].contentWindow.draw_chart_b(eval('bar_data_b'))
+}, 1000)
--- deploy/app/scripts/prepare_page.js
+++ deploy/app/scripts/prepare_page.js
@@ -152,13 +152,18 @@ style_layout('section_1_visuals', 1, {
 add_dropdown('section_1_visuals_cell', 1, {
     "this_class" : "select_passenger",
     "title" : "select passenger...",
-    "options" : ['','','']
+    "options" : ['bar_data_a','bar_data_b','bar_data_c', 'bar_data_d']
 })
 
 style_dropdown('select_passenger', 1, {
     "width" : "200px"
 })
 
+add_event('select_passenger', 1, {
+    "type" : "change",
+    "function" : "$('.bar_chart_frame')[0].contentWindow.draw_chart(eval(this.value)); toggle_functions('smile_one()', 'smile_two()')"
+})
+/*
 add_text('section_1_visuals_cell', 2, {
     "this_class" : "profile_viz_title",
     "text" : "PASSENGER PROFILE"
@@ -168,13 +173,15 @@ style_text('profile_viz_title', 1, {
     "color" : "white",
     "font-size" : "20px"
 })
+*/
 
 add_iframe("section_1_visuals_cell", 2, {
 	"this_class" : "bar_chart_frame",
 	"source" : "d3/bar_chart.html"
 })
 style_iframe('bar_chart_frame', 1, {
-    "width" : "100%"
+    "width" : "100%",
+    "margin-left" : "50px"
 })
 $(".bar_chart_frame")[0].setAttribute("scrolling", "no");
 
@@ -186,6 +193,162 @@ add_image('section_1_visuals_cell', 3, {
 style_image('show_smiles', 1, {
     "width" : "160px"
 })
+
+// *********** SECTION 2
+
+clone_layout("sections", 2, {
+    "copy_class" : "hold_section_title",
+    "copy_instance" : 1,
+    "this_class" : "hold_section_title_b",
+    "row_class" : "hold_section_title_b_row",
+    "cell_class" : "hold_section_title_b_cell"
+    })
+
+remove_element('section_title', 2)
+
+add_text('hold_section_title_b_cell', 1, {
+    "this_class" : "section_title",
+    "text" : "PROFILE TESTER"
+})
+
+
+clone_layout("sections", 2, {
+    "copy_class" : "section_1_visuals",
+    "copy_instance" : 1,
+    "this_class" : "section_2_visuals",
+    "row_class" : "section_2_visuals_row",
+    "cell_class" : "section_2_visuals_cell"
+    })
+
+style_layout('section_2_visuals', 1, {
+    "border" : 0
+})
+
+delay_event({
+"function" : `
+style_layout('section_2_visuals_cell', 1, {
+    "valign" : "center",
+    "halign" : "center"
+})
+`
+})
+
+remove_element('select_passenger', 2)
+
+add_dropdown('section_2_visuals_cell', 1, {
+    "this_class" : "select_random_passenger",
+    "title" : "select random passenger...",
+    "options" : ['bar_data_a','bar_data_b','bar_data_c', 'bar_data_d']
+})
+
+style_dropdown('select_random_passenger', 1, {
+    "width" : "220px"
+})
+
+add_event('select_random_passenger', 1, {
+    "type" : "change",
+    "function" : "$('.bar_chart_frame_b')[0].contentWindow.draw_chart(eval(this.value))"
+})
+
+remove_element('bar_chart_frame', 2)
+
+add_iframe("section_2_visuals_cell", 2, {
+	"this_class" : "bar_chart_frame_b",
+	"source" : "d3/bar_chart_b.html"
+})
+style_iframe('bar_chart_frame_b', 1, {
+    "width" : "100%",
+    "margin-left" : "50px"
+})
+$(".bar_chart_frame_b")[0].setAttribute("scrolling", "no");
+
+
+remove_element('show_smiles', 2)
+
+add_text('section_2_visuals_cell', 3, {
+    "this_class" : "make_choice_title",
+    "text" : "<br><br>MAKE YOUR CHOICE"
+})
+
+style_text('make_choice_title', 1, {
+    "color" : "white",
+    "font-size" : "20px"
+})
+
+style_layout('section_2_visuals_cell', 3, {
+    "halign" : "center"
+})
+
+add_layout('section_2_visuals_cell', 3, {
+    "this_class" : "hold_make_choice_buttons",
+    "row_class" : "hold_make_choice_buttons_row",
+    "cell_class" : "hold_make_choice_buttons_cell",
+    "number_of_columns" : 1,
+    "number_of_rows" : 3
+})
+
+style_layout('hold_make_choice_buttons', 1, {
+    "height" : "200px",
+    "border" : 0
+})
+
+add_button('hold_make_choice_buttons_cell', 1, {
+    "this_class" : "hold_make_choice_button",
+    "text" : "SURVIVES"
+})
+
+add_button('hold_make_choice_buttons_cell', 2, {
+    "this_class" : "hold_make_choice_button",
+    "text" : "DOES NOT SURVIVE"
+})
+add_button('hold_make_choice_buttons_cell', 3, {
+    "this_class" : "hold_make_choice_button",
+    "text" : "ASK AI"
+})
+
+delay_event({
+"function" : `
+style_button('hold_make_choice_button', 1, {
+"background" : "rgba(145, 255, 94, 0.69)"
+})
+add_event('hold_make_choice_button', 1, {
+"type" : "click",
+"function" : "animate_element('hold_make_choice_button', 1, {'type' : 'spin'})"
+})
+style_button('hold_make_choice_button', 2, {
+"background" : "rgba(255, 24, 78, 0.97)"
+})
+add_event('hold_make_choice_button', 2, {
+"type" : "click",
+"function" : "animate_element('hold_make_choice_button', 2, {'type' : 'hinge'})"
+})
+style_button('hold_make_choice_button', 3, {
+"background" : "rgba(47, 206, 255, 0.88)"
+})
+add_event('hold_make_choice_button', 3, {
+"type" : "click",
+"function" : "animate_element('hold_make_choice_button', 3, {'type' : 'spin'})"
+})
+`
+})
+
+// *********** SECTION 3
+
+add_text('sections', 3, {
+    "this_class" : "section_title",
+    "text" : "SCOREBOARD"
+})
+
+add_iframe("sections", 3, {
+	"this_class" : "bar_chart_frame_c",
+	"source" : "d3/bar_chart_c.html"
+})
+style_iframe('bar_chart_frame_c', 1, {
+    "width" : "1000px",
+    "height" : "300px"
+})
+
+$(".bar_chart_frame_c")[0].setAttribute("scrolling", "no");
 
 // *********** ALL_ STYLES
 all_style_text('section_title', {
@@ -201,9 +364,25 @@ all_style_button('option_button', {
 all_style_layout("section_1_visuals_cell", {
     "valign" : "top"
 })
+all_style_button('hold_make_choice_button', {
+    "background" : "orangered",
+    "width" : "80%"
+})
+all_style_layout("hold_make_choice_buttons_cell", {
+    "halign" : "center"
+})
 
 // ******* PRECEDENCE STYLES
 style_layout('section_1_visuals_cell', 3, {
     "halign" : "center",
     "valign" : "center"
 })
+
+// *********** UTILITY FUNCTIONS
+function smile_one() {
+    $('.show_smiles').attr('src', 'img/smile_1.png')
+    }
+
+function smile_two() {
+    $('.show_smiles').attr('src', 'img/smile_2.png')
+    }
